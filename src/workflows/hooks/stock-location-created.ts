55aa6bd5d10c6add39606214252ec003c2dbb681
IgorKhomenko <khomenkoigor+1@gmail.com>
2025-06-10 10:32:44 +0300

    fix build issue

--- src/workflows/hooks/stock-location-created.ts
+++ src/workflows/hooks/stock-location-created.ts
@@ -1,18 +1,18 @@
-import { StoreDTO, UserDTO } from "@medusajs/framework/types";
+import { StoreDTO } from "@medusajs/framework/types";
 import { createStockLocationsWorkflow } from "@medusajs/medusa/core-flows";
 import { linkStockLocationToStoreWorkflow } from "../link-stock-location-to-store";
 
-createStockLocationsWorkflow.hooks.stockLocationsCreated(async ({ stockLocations }, { container }) => {
-  console.log("HOOK stockLocationsCreated", stockLocations);
+createStockLocationsWorkflow.hooks.stockLocationsCreated(
+  async ({ stockLocations }, { container }) => {
+    console.log("HOOK stockLocationsCreated", stockLocations);
 
-  const loggedInUser = container.resolve("loggedInUser") as UserDTO;
-  const currentStore = container.resolve("currentStore") as StoreDTO;
-  if (loggedInUser)
-    await linkStockLocationToStoreWorkflow(container).run({
-      input: {
-        stockLocations,
-        userId: loggedInUser.id,
-        storeId: currentStore.id,
-      },
-    });
-});
+    const currentStore = container.resolve("currentStore") as StoreDTO;
+    if (currentStore)
+      await linkStockLocationToStoreWorkflow(container).run({
+        input: {
+          stockLocations,
+          storeId: currentStore.id,
+        },
+      });
+  }
+);

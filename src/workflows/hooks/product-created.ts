3f7c741949c8bd3ee3a2bb4c2db5e3be6c7cfd14
Viktoriia <107419074+belvikuha@users.noreply.github.com>
2025-06-18 10:38:14 +0300

    Multi store feature: fix links (#32)
    - Improve Multi-store capabilities

--- src/workflows/hooks/product-created.ts
+++ src/workflows/hooks/product-created.ts
@@ -1,21 +1,19 @@
 import { createProductsWorkflow } from "@medusajs/medusa/core-flows";
-import { UserDTO } from "@medusajs/framework/types";
+import { StoreDTO } from "@medusajs/framework/types";
 import { linkProductToStoreWorkflow } from "../link-product-to-store";
 
-createProductsWorkflow.hooks.productsCreated(
-  async ({ products }, { container }) => {
-    console.log("HOOK productsCreated", products);
+createProductsWorkflow.hooks.productsCreated(async ({ products }, { container }) => {
+  console.log("HOOK productsCreated", products);
 
-    const loggedInUser = container.resolve("loggedInUser") as UserDTO;
-    await Promise.all(
-      products.map(({ id }) =>
-        linkProductToStoreWorkflow(container).run({
-          input: {
-            productId: id,
-            userId: loggedInUser.id,
-          },
-        })
-      )
-    );
-  }
-);
+  const currentStore = container.resolve("currentStore") as StoreDTO;
+  await Promise.all(
+    products.map(({ id }) =>
+      linkProductToStoreWorkflow(container).run({
+        input: {
+          productId: id,
+          storeId: currentStore.id,
+        },
+      })
+    )
+  );
+});

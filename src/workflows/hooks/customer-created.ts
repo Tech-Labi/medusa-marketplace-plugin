00efbd61e18db7e5c68395d3317e71da5788e703
Vladyslav Surkov <vlads@connectycube.com>
2025-07-15 17:33:47 +0300

    fix: customersCreated hook

--- src/workflows/hooks/customer-created.ts
+++ src/workflows/hooks/customer-created.ts
@@ -5,16 +5,17 @@ createCustomersWorkflow.hooks.customersCreated(
   async ({ customers }, { container }) => {
     console.log("HOOK customersCreated", customers);
 
-  if (process.env.IS_CHANNEL_PRICING_ENABLED) {
-    customers.forEach(({ id, metadata }) => {
-      if (metadata?.sales_channel_id) {
-        linkCustomersToSalesChannelWorkflow(container).run({
-          input: {
-            customer_id: id,
-            sales_channel_id: metadata.sales_channel_id as string,
-          },
-        });
-      }
-    });
+    if (process.env.IS_CHANNEL_PRICING_ENABLED) {
+      customers.forEach(({ id, metadata }) => {
+        if (metadata?.sales_channel_id) {
+          linkCustomersToSalesChannelWorkflow(container).run({
+            input: {
+              customer_id: id,
+              sales_channel_id: metadata.sales_channel_id as string,
+            },
+          });
+        }
+      });
+    }
   }
 );

58d0054c4687dd506a727e9d59b980de37e969bc
IgorKhomenko <70977170+IgorKhomenko@users.noreply.github.com>
2025-11-26 11:32:16 +0200

    Merge pull request #86 from Tech-Labi/refactor-addStoreIdToFilterableFields
    secure add-store-id-to-filterable-fields.ts

--- src/api/middlewares/add-store-id-to-filterable-fields.ts
+++ src/api/middlewares/add-store-id-to-filterable-fields.ts
@@ -1,11 +1,19 @@
-import { type MedusaNextFunction, type MedusaRequest, type MedusaResponse } from "@medusajs/framework/http";
+import {
+  type MedusaNextFunction,
+  type MedusaRequest,
+  type MedusaResponse,
+} from "@medusajs/framework/http";
 import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
 import { UserDTO } from "@medusajs/framework/types";
 import { SUPER_ADMIN_STORE_NAME } from "../../constants";
 
 import * as cookie from "cookie";
 
-export async function addStoreIdToFilterableFields(req: MedusaRequest, res: MedusaResponse, next: MedusaNextFunction) {
+export async function addStoreIdToFilterableFields(
+  req: MedusaRequest,
+  res: MedusaResponse,
+  next: MedusaNextFunction
+) {
   const cookies = req.headers.cookie ? cookie.parse(req.headers.cookie) : {};
   const storeId = cookies?.store_id;
 
@@ -13,7 +21,12 @@ export async function addStoreIdToFilterableFields(req: MedusaRequest, res: Medu
     allowUnregistered: true,
   }) as UserDTO;
   if (!loggedInUser) {
-    return next();
+    // TODO: allow to call this via API (not only admin)
+    res.status(403).json({
+      type: "invalid_request_error",
+      message: "You do not have access to any stores.",
+    });
+    return;
   }
 
   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
@@ -29,16 +42,27 @@ export async function addStoreIdToFilterableFields(req: MedusaRequest, res: Medu
     req.filterableFields = {};
   }
   if (stores?.length > 0) {
-    const id = stores.find((store) => store.store_id === storeId) ? storeId : stores[0].store_id;
+    const id = stores.find((store) => store.store_id === storeId)
+      ? storeId
+      : stores[0].store_id;
     if (req.url.includes("/admin/stores") && req.method === "GET") {
       req.filterableFields["id"] = id;
     } else {
       // set 'filterableFields' so then the 'maybeApplyLinkFilter' middleware will process it
       req.filterableFields["store_id"] = id;
     }
-  } // super admin?
-  else if (req.url.includes("/admin/stores") && req.method === "GET") {
-    req.filterableFields["name"] = SUPER_ADMIN_STORE_NAME;
+    // super admin?
+  } else if (loggedInUser.metadata?.is_super_admin) {
+    if (req.url.includes("/admin/stores") && req.method === "GET") {
+      req.filterableFields["name"] = SUPER_ADMIN_STORE_NAME;
+    }
+  } else {
+    // user w/o stores - should not have access
+     if (req.url.includes("/admin/stores") && req.method === "GET") {
+       req.filterableFields["id"] = [];
+     } else {
+       req.filterableFields["store_id"] = [];
+     }
   }
 
   return next();

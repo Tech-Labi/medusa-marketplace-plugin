d8072b22e25af34659a31b71d60b76c314c7a279
Vladyslav Surkov <vlads@connectycube.com>
2025-12-18 22:37:49 +0200

    feat: added extraction of userId from the api key

--- src/api/middlewares/logged-in-user.ts
+++ src/api/middlewares/logged-in-user.ts
@@ -1,20 +1,41 @@
 import type {
   MedusaNextFunction,
-  MedusaRequest,
+  AuthenticatedMedusaRequest,
   MedusaResponse,
 } from "@medusajs/framework/http";
 import { Modules } from "@medusajs/framework/utils";
-import { IUserModuleService } from "@medusajs/framework/types";
-import { container } from "@medusajs/framework"
+import {
+  IUserModuleService,
+  IApiKeyModuleService,
+} from "@medusajs/framework/types";
 
 export async function registerLoggedInUser(
-  req: MedusaRequest,
+  req: AuthenticatedMedusaRequest,
   res: MedusaResponse,
   next: MedusaNextFunction
 ) {
+  const allowApiKeysForVendors =
+    process.env.ALLOW_API_KEYS_FOR_VENDORS == "true";
+  let userIdFromApiKey;
+
+  if (
+    allowApiKeysForVendors &&
+    req.auth_context?.actor_type === "api-key" &&
+    req.auth_context?.actor_id
+  ) {
+    const apiKeyId = req.auth_context?.actor_id;
+    const apiKeyService = req.scope.resolve<IApiKeyModuleService>(
+      Modules.API_KEY
+    );
+    const apiKey = await apiKeyService.retrieveApiKey(apiKeyId);
+    userIdFromApiKey = apiKey.created_by;
+  }
+
   const userModuleService: IUserModuleService = req.scope.resolve(Modules.USER);
   const userId =
-    req.session?.impersonate_user_id || req.session?.auth_context?.actor_id;
+    req.session?.impersonate_user_id ||
+    req.session?.auth_context?.actor_id ||
+    userIdFromApiKey;
   if (userId) {
     const user = await userModuleService.retrieveUser(userId);
     req.scope.register({
